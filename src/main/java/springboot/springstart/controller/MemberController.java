3833185283f0f8258d90c6f080a7f0affbc87533
sjh9391985 <sjh9391985@gmail.com>
2022-08-31 10:15:49 +0900

    회원가입

--- src/main/java/springboot/springstart/controller/MemberController.java
+++ src/main/java/springboot/springstart/controller/MemberController.java
@@ -2,6 +2,9 @@ package springboot.springstart.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import springboot.springstart.domain.Member;
 import springboot.springstart.service.MemberService;
 
 @Controller
@@ -12,6 +15,20 @@ public class MemberController {
     @Autowired // DI 의존성 주입
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createFoem(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+
+        return "redirect:/";
     }
 
 }
